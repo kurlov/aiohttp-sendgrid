b8cd40c23e967fd022800602e2da1c6a11156443
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2018-01-02 23:56:55 +0500

    Added example post request

---
+++ aiohttp_sendgrid/__init__.py
@@ -0,0 +1,45 @@
+import aiohttp
+import asyncio
+import os
+
+
+API_KEY = os.environ.get('SENDGRID_API_KEY', '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')
+API_URL = 'https://api.sendgrid.com/v3/mail/send'
+auth = 'Bearer ' + str(API_KEY)
+headers = {'content-type': 'application/json', 'authorization': auth}
+loop = asyncio.get_event_loop()
+
+async def fetch(session, url, payload):
+    async with session.post(url, data=payload, headers=headers) as response:
+        return await response.json()
+
+async def send(payload):
+    async with aiohttp.ClientSession(loop=loop) as session:
+        response = await fetch(session, API_URL, payload)
+        print(response)
+
+if __name__ == '__main__':
+    data = {"personalizations": [
+    {
+      "to": [
+        {
+          "email": "sasha-kurlov@ya.ru",
+          "name": "Sasha"
+        }
+      ],
+      "subject": "Hello, World!"
+    }
+  ],
+  "from": {
+    "email": "sam.smith@example.com",
+    "name": "Sam Smith"
+  },
+  "subject": "Hello, World!",
+  "content": [
+    {
+      "type": "text/html",
+      "value": "<html><p>Hello, world!</p></html>"
+    }
+  ]
+}
+    loop.run_until_complete(send(data))
