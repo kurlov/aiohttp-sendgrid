b030e15eb6c5b1aa71199e974b0ba8750c81ec39
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2018-01-04 04:54:33 +0500

    Updated data schema example

--- aiohttp_sendgrid/__init__.py
+++ aiohttp_sendgrid/__init__.py
@@ -6,7 +6,7 @@ import os
 API_KEY = os.environ.get('SENDGRID_API_KEY')
 API_URL = 'https://api.sendgrid.com/v3/mail/send'
 auth = 'Bearer ' + str(API_KEY)
-headers = {'content-type': 'application/json', 'authorization': auth}
+headers = {'authorization': auth}
 loop = asyncio.get_event_loop()
 
 async def fetch(session, url, payload):
@@ -22,26 +22,13 @@ async def send(payload):
         print(response)
 
 if __name__ == '__main__':
-    data = {"personalizations": [
-    {
-      "to": [
-        {
-          "email": "sasha-kurlov@ya.ru",
-          "name": "Sasha"
-        }
-      ],
-      "subject": "Hello, World!"
-    }
-  ],
+    data = {'personalizations': [{"to": [{"email": "sasha-kurlov@ya.ru"}]}],
   "from": {
     "email": "sam.smith@example.com",
     "name": "Sam Smith"
   },
   "subject": "Hello, World!",
-  "content": [
-    {
-      "type": "text/html",
-      "value": "<html><p>Hello, world!</p></html>"
+  "content": [{"type": "text/html", "value": "<html><p>Hello, world!</p></html>"
     }
   ]
 }
