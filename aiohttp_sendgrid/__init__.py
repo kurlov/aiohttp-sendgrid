3364f14b1188ea1996a04d771faece111533cdd7
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2018-01-06 10:17:42 +0500

    Deleted redundant _main__ block

--- aiohttp_sendgrid/__init__.py
+++ aiohttp_sendgrid/__init__.py
@@ -1,12 +1,3 @@
-from sendgrid import Sendgrid
+from .sendgrid import Sendgrid
 
-
-if __name__ == '__main__':
-    import asyncio
-    mailer = Sendgrid()
-    loop = asyncio.get_event_loop()
-    data = {'to': 'sasha-kurlov@ya.ru',
-            'sender': 'sasha-kurlov@ya.ru',
-            'subject': 'greetings',
-            'content': '<h1>Hello</h1>'}
-    loop.run_until_complete(mailer.send(**data))
+__all__ = ['Sendgrid']
