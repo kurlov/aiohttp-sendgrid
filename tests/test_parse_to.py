8f380f59da83238114663425ae9ac605825ffc9f
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2018-01-08 06:33:23 +0500

    Added basic tests

---
+++ tests/test_parse_to.py
@@ -0,0 +1,15 @@
+from aiohttp_sendgrid import Sendgrid
+
+
+def test_generate_email_no_name():
+    email = 'name@example.com'
+    generated_email = Sendgrid._generate_email(email)
+    assert generated_email == {'email': 'name@example.com'}
+
+
+def test_generate_email_with_name():
+    email = 'name@example.com'
+    name = 'Ivan'
+    generated_email = Sendgrid._generate_email(email, name)
+    assert generated_email == {'email': 'name@example.com',
+                               'name': 'Ivan'}
