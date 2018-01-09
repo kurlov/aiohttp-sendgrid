ae14d87c71be7a146c1f6286ee4f3c0bdbcfb41b
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2018-01-09 06:31:29 +0500

    Added parse tests

--- tests/test_parse_to.py
+++ tests/test_parse_to.py
@@ -1,15 +1,59 @@
 from aiohttp_sendgrid import Sendgrid
 
 
-def test_generate_email_no_name():
-    email = 'name@example.com'
-    generated_email = Sendgrid._generate_email(email)
-    assert generated_email == {'email': 'name@example.com'}
+def test_parse_to_str():
+    data = 'name@example.com'
+    parsed_email = Sendgrid()._parse_to_emails(data)
+    assert parsed_email == [{'email': 'name@example.com'}]
 
 
-def test_generate_email_with_name():
-    email = 'name@example.com'
-    name = 'Ivan'
-    generated_email = Sendgrid._generate_email(email, name)
-    assert generated_email == {'email': 'name@example.com',
-                               'name': 'Ivan'}
+def test_parse_to_dict():
+    data = {'email': 'name@example.com', 'name': 'Ivan'}
+    parsed_email = Sendgrid()._parse_to_emails(data)
+    assert parsed_email == [{'email': 'name@example.com', 'name': 'Ivan'}]
+
+
+def test_parse_to_dict_no_name():
+    data = {'email': 'name@example.com'}
+    parsed_email = Sendgrid()._parse_to_emails(data)
+    assert parsed_email == [{'email': 'name@example.com'}]
+
+
+def test_parse_to_list_tuple_of_str():
+    data = ['name1@example.com', 'name2@example.com', 'name3@example.com']
+    result = [{'email': 'name1@example.com'},
+              {'email': 'name2@example.com'},
+              {'email': 'name3@example.com'}]
+    parsed_list = Sendgrid()._parse_to_emails(data)
+    assert parsed_list == result
+    data_tuple = tuple(data)
+    parsed_tuple = Sendgrid()._parse_to_emails(data_tuple)
+    assert parsed_tuple == result
+
+
+def test_parse_to_list_tuple_of_dict():
+    data = [{'email': 'name1@example.com', 'name': 'Ivan1'},
+            {'email': 'name2@example.com', 'name': 'Ivan2'},
+            {'email': 'name3@example.com', 'name': 'Ivan3'}]
+    result = [{'email': 'name1@example.com', 'name': 'Ivan1'},
+              {'email': 'name2@example.com', 'name': 'Ivan2'},
+              {'email': 'name3@example.com', 'name': 'Ivan3'}]
+    parsed_list = Sendgrid()._parse_to_emails(data)
+    assert parsed_list == result
+    data_tuple = tuple(data)
+    parsed_tuple = Sendgrid()._parse_to_emails(data_tuple)
+    assert parsed_tuple == result
+
+
+def test_parse_to_list_tuple_heterogeneous():
+    data = [{'email': 'name1@example.com', 'name': 'Ivan1'},
+            {'email': 'name2@example.com'},
+            {'email': 'name3@example.com', 'name': 'Ivan3'}]
+    result = [{'email': 'name1@example.com', 'name': 'Ivan1'},
+              {'email': 'name2@example.com'},
+              {'email': 'name3@example.com', 'name': 'Ivan3'}]
+    parsed_list = Sendgrid()._parse_to_emails(data)
+    assert parsed_list == result
+    data_tuple = tuple(data)
+    parsed_tuple = Sendgrid()._parse_to_emails(data_tuple)
+    assert parsed_tuple == result
