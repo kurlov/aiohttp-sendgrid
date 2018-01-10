cd01c96a7fd212efb5c42e18ab7f18605396ae64
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2018-01-11 01:42:14 +0500

    Added ValueError and TypeError tests

--- tests/test_parse_from.py
+++ tests/test_parse_from.py
@@ -1,4 +1,5 @@
 from aiohttp_sendgrid import Sendgrid
+import pytest
 
 
 def test_parse_from_str():
@@ -17,3 +18,14 @@ def test_parse_from_dict_no_name():
     data = {'email': 'name@example.com'}
     parsed_email = Sendgrid()._parse_from_email(data)
     assert parsed_email == {'email': 'name@example.com'}
+
+
+def test_parse_from_wrong_input():
+    no_email = {'hello': 'world'}
+    with pytest.raises(TypeError):
+        Sendgrid()._parse_from_email(no_email)
+    integer = 1
+    val_set = {1}
+    with pytest.raises(ValueError):
+        Sendgrid()._parse_from_email(integer)
+        Sendgrid()._parse_from_email(val_set)
--- tests/test_parse_to.py
+++ tests/test_parse_to.py
@@ -1,4 +1,5 @@
 from aiohttp_sendgrid import Sendgrid
+import pytest
 
 
 def test_parse_to_str():
@@ -57,3 +58,18 @@ def test_parse_to_list_tuple_heterogeneous():
     data_tuple = tuple(data)
     parsed_tuple = Sendgrid()._parse_to_emails(data_tuple)
     assert parsed_tuple == result
+
+
+def test_parse_to_wrong_input():
+    no_email = {'hello': 'world'}
+    with pytest.raises(TypeError):
+        Sendgrid()._parse_to_emails(no_email)
+    integer = 1
+    val_set = {1}
+    invalid_list = ['name@example.com', 1]
+    invalid_tuple = ('name@example.com', 1)
+    with pytest.raises(ValueError):
+        Sendgrid()._parse_to_emails(integer)
+        Sendgrid()._parse_to_emails(val_set)
+        Sendgrid()._parse_to_emails(invalid_list)
+        Sendgrid()._parse_to_emails(invalid_tuple)
